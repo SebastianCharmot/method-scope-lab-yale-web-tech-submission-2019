8ef85f19da8ebc54987fc9d362a124b42b82cb6a
crisp-panel-7711 <secharmot@davidson.edu>
2019-01-19 20:34:17 +0000

    Automatically backed up by Learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,3 +1,7 @@
-def catch_phrase(phrase = "It's-a me, Mario!")
-  puts phrase
+#def catch_phrase(phrase = "It's-a me, Mario!")
+  #puts phrase
+#end
+def catch_phrase
+  catch_phrase = "It's-a me, Mario!"
+  puts catch_phrase
 end
